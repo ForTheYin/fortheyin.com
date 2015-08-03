2c1ac7e03e57becb27fe462ad0f407fd75432b39
Andy Yin <dev@fortheyin.com>
2015-08-02 23:46:12 -0400

    removed angular

--- public/app/app.js
+++ public/app/app.js
@@ -1,21 +1,32 @@
-(function (){
-  'use strict';
+(function () {
+  var defaultText = '';
 
-  var MainCtrl = function ($http, $scope, $interval) {
-   $scope.token = {};
-   $scope.inputBlock = '';
+  $( document ).ready(function () {
+    var staggerText = function (text, speed) {
+      speed = typeof speed === 'undefined' ? 200 : speed;
+      var remaining = text;
+      var pushed = '';
 
-   $http.get('/api/user').then(function (res) {
-     $scope.token = res.data;
-   });
+      var interval = setInterval(function () {
+        if (!remaining) return clearInterval(interval);
+        pushed += remaining[0];
+        remaining = remaining.substring(1);
+        $( '#terminal-text' ).text(defaultText + pushed);
+      }, speed);
+    };
 
-   $interval(function (){
-     $scope.inputBlock = $scope.inputBlock ? '' : '#';
-   }, 500);
+    $.ajax({
+      url: '/api/user',
+      success: function (data) {
+        defaultText = 'fortheyin ' + data.ip + '$ ';
+        $( '#terminal-text' ).text(defaultText);
+        staggerText('hello', 200);
+      }
+    });
 
-  };
-
-  angular.
-    module('fortheyin', ['ngAnimate']).
-    controller('MainCtrl', ['$http', '$scope', '$interval', MainCtrl]);
+    setInterval(function () {
+      var text = $('#terminal-block').text() ? '' : '#';
+      $( '#terminal-block' ).text(text);
+    }, 300);
+  });
 })();
